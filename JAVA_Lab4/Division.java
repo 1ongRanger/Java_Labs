ccbc774418f6fa054e87c920a664564abbe3c598
1ongRanger <92697127+1ongRanger@users.noreply.github.com>
2022-12-09 20:24:25 +0300

    Add files via upload

---
+++ JAVA_Lab4/Division.java
@@ -0,0 +1,22 @@
+public class Division
+{
+    private static int divisionID = 0;
+    private int id;
+    private String title;
+
+    public Division(String title)
+    {
+        this.id = divisionID++;
+        this.title = title;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getDivision()
+    {
+        return "ID = " + this.id + ", Title " + this.title;
+    }
+}
