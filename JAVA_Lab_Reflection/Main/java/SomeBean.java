055deaf17700175940640ba8f7536b0051f55f8c
1ongRanger <92697127+1ongRanger@users.noreply.github.com>
2022-12-23 17:37:14 +0300

    Add files via upload

---
+++ JAVA_Lab_Reflection/Main/java/SomeBean.java
@@ -0,0 +1,15 @@
+import somePackage.SomeInterface;
+import somePackage.SomeOtherInterface;
+
+public class SomeBean {
+    @AutoInjectable
+    private SomeInterface field1;
+
+    @AutoInjectable
+    private SomeOtherInterface field2;
+
+    public void foo(){
+        field1.doSomething();
+        field2.doSomeOther();
+    }
+}
